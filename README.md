115a962ffbfb8b1aa3b6955eb90e5ae8d92e0b7b
YouniS Bensalah <younis.bensalah@gmail.com>
2017-10-18 08:03:39 +0200

    Proof of concept

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
 # afkfi.sh
-afk fishing script for minecraft
+
+afk fishing script for Minecraft
+
+## Prerequisites
+
+0. Make sure you're running PulseAudio and Xorg.
+
+1. Make sure `xdotool` is installed.
+
+2. Create a file called `~/.alsoftrc` and add the following lines:  
+```
+[pulse]
+allow-moves=yes
+```
+
+3. (Re)start Minecraft
+
+## Usage
+
+0. Disable all Minecraft sounds except for _Friendly Creatures_.
+
+1. Go to a safe (i.e., no hostile mobs who could kill you while afk) and quiet (i.e., no animal noises) fishing place in-game, aim the fishing rod at the water (without casting the line yet), and hit `Esc`.
+
+2. Run the `afkfi.sh` script.
+
+3. ???
+
+4. Profit!
---
+++ afkfi.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+echo
+echo "afkfi.sh by 0x4"
+echo
+
+xdotool version &>/dev/null || { echo "You need to install xdotool."; exit 1; }
+
+WID=$(xdotool search --sync --onlyvisible --name Minecraft)
+[ -z "$WID" ] && { echo "Failed to retrieve window id."; exit 1; }
+
+SINKINPUT=$(pacmd list-sink-inputs | grep 'index:' | sed 's/\s*index: //' | sed $(pacmd list-sink-inputs | grep 'application.process.binary =' | grep -n 'application.process.binary = "java"' | cut -d: -f1)'q;d')
+[ -z "$SINKINPUT" ] && { echo "Failed to retrieve sink input index."; exit 1; }
+
+SINK=$(pacmd list-sink-inputs | grep 'sink:' | sed 's/\s*sink: //' | cut -d'<' -f1 | sed $(pacmd list-sink-inputs | grep 'application.process.binary =' | grep -n 'application.process.binary = "java"' | cut -d: -f1)'q;d')
+
+pactl list short sinks | grep -q afkfish || (pactl load-module module-combine-sink slaves="$SINK" sink_name=afkfish sink_properties=device.description="afkfi.sh" && pactl move-sink-input "$SINKINPUT" afkfish)
+
+xdotool windowactivate --sync "$WID" && xdotool key --window "$WID" Escape && sleep 0.5 && xdotool click --window "$WID" 3
+
+while :; do
+    PEAK=$(parec -d afkfish.monitor --channels=1 --latency=2 2>/dev/null | od -N2 -td2 | head -n1 | cut -d' ' -f2- | tr -d ' ')
+    if [ "$PEAK" -ge 100 -o "$PEAK" -le -100 ]; then
+        echo "Blub blub blub...oo0O"
+        xdotool click --window "$WID" --repeat 2 --delay 1000 3 && sleep 1
+        sleep 0.1
+    else
+        echo "Zzzz..."
+    fi
+done
